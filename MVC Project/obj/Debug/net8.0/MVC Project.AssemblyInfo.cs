672ce74fd7351431319c29f6c4955623c04cde59
Mohamed Abdelhakeem <mohamed.abdelhakeemx@gmail.com>
2025-04-24 17:03:23 +0200

    Footer Added Successfully!

--- MVC Project/obj/Debug/net8.0/MVC Project.AssemblyInfo.cs
+++ MVC Project/obj/Debug/net8.0/MVC Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25d27e3e48043fefd5ce365c5b015c2ae3f6620c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f342c8c628b37eb37647bc5ec67341c38a7bf1fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
